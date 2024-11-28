5c51e017342b8b07a13b6635cdc4c5c063483670
j8xixo12 <as2266317@gmail.com>
2024-11-28 22:49:25 +0800

    update draft program

--- app/program/page.tsx
+++ app/program/page.tsx
@@ -11,40 +11,39 @@ const Page = () => {
   return (
     <Article>
       <div className="tw-flex tw-flex-col tw-gap-6 tablet:tw-gap-8">
-        <ScheduleTime time="09:30" />
+        <ScheduleTime time="09:00" />
         <TalkInfoCard
           talk={{
             title: "Check-in",
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              "",
+            room: "All",
+            start: "9:00",
+            end: "9:30",
+          }}
+          variant="general"
+        />
+        <ScheduleTime time="09:30" />
+        <TalkInfoCard
+          talk={{
+            title: "Opening",
+            description:
+              "",
             room: "All",
             start: "9:30",
-            end: "10:00",
+            end: "9:50",
           }}
           variant="general"
         />
         <ScheduleTime time="10:00" />
         <TalkInfoCard
           talk={{
-            title: "Opening",
-            description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            room: "All",
-            persons: ["Yung-Yu Chen"],
-            start: "10:00",
-            end: "10:20",
-          }}
-          variant="general"
-        />
-        <ScheduleTime time="10:30" />
-        <TalkInfoCard
-          talk={{
             title: "Introduction - cyntx",
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            room: "Talks",
-            start: "10:30",
-            end: "10:40",
+              "",
+            room: "R716",
+            start: "10:00",
+            end: "10:10",
           }}
           variant="talks"
         />
@@ -52,33 +51,34 @@ const Page = () => {
           talk={{
             title: "Introduction - modmesh",
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            room: "Talks",
-            start: "10:45",
-            end: "10:55",
+              "Introduce modmesh: a system to solve problems that can be meshed",
+            room: "R716",
+            persons: ["Yung-Yu Chen"],
+            start: "10:15",
+            end: "10:25",
           }}
           variant="talks"
         />
         <TalkInfoCard
           talk={{
-            title: "Training",
+            title: "NASA TOPS Training",
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            room: "DUTC",
-            start: "10:30",
+              "",
+            room: "R833",
+            start: "10:00",
             end: "12:00",
           }}
           variant="training"
         />
-        <ScheduleTime time="11:00" />
         <TalkInfoCard
           talk={{
             title: "Introduction - swprotal",
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            room: "Talks",
-            start: "11:00",
-            end: "11:10",
+              "Building the sciwork infrastructure",
+            room: "R716",
+            persons: ["Chester Cheng"],
+            start: "10:30",
+            end: "10:40",
           }}
           variant="talks"
         />
@@ -86,22 +86,24 @@ const Page = () => {
           talk={{
             title: "Introduction - Altlas",
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            room: "Talks",
-            start: "11:25",
-            end: "11:30",
+              "Language, Knowledge and Computation: the Quest to Divide and Conquer to Strong AI",
+            room: "R716",
+            persons: ["Peter Wolf"],
+            start: "10:50",
+            end: "11:00",
           }}
           variant="talks"
         />
-        <ScheduleTime time="11:30" />
+        <ScheduleTime time="11:00" />
         <TalkInfoCard
           talk={{
             title: "Introduction - utensor",
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            room: "Talks",
-            start: "11:35",
-            end: "11:45",
+              "Deep Learning on Tiny Devices: Review and Outlook for uTensor",
+            room: "R716",
+            persons: ["Dboy Liao"],
+            start: "11:05",
+            end: "11:15",
           }}
           variant="talks"
         />
@@ -109,9 +111,22 @@ const Page = () => {
           talk={{
             title: "Introduction - Pydoc-zhtw",
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            room: "Talks",
-            start: "11:50",
+              "",
+            room: "R716",
+            start: "11:20",
+            end: "11:30",
+          }}
+          variant="talks"
+        />
+        <ScheduleTime time="11:30" />
+        <TalkInfoCard
+          talk={{
+            title: "Talk: Training-Free Scanning Robustness Guided Diffusion Model for Aesthetic QR Code Generation",
+            description:
+              "",
+            room: "R716",
+            persons: ["Jia-Wei"],
+            start: "11:40",
             end: "12:00",
           }}
           variant="talks"
@@ -129,10 +144,10 @@ const Page = () => {
         <ScheduleTime time="13:30" />
         <TalkInfoCard
           talk={{
-            title: "Training",
+            title: "NASA TOPS Training",
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            room: "DUTC",
+              "",
+            room: "R833",
             start: "13:30",
             end: "15:00",
           }}
@@ -142,20 +157,31 @@ const Page = () => {
           talk={{
             title: "Sprint Booths",
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            room: "Booths",
+              "Discussion some topics with project leader",
+            room: "R815",
             start: "13:30",
             end: "15:00",
           }}
           variant="booths"
         />
+          <TalkInfoCard
+          talk={{
+            title: "Tea break",
+            description:
+              "Take a break!",
+            room: "All",
+            start: "15:00",
+            end: "16:00",
+          }}
+          variant="general"
+        />
         <ScheduleTime time="16:00" />
         <TalkInfoCard
           talk={{
-            title: "Training",
+            title: "NASA TOPS Training",
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            room: "DUTC",
+              "",
+            room: "R833",
             start: "16:00",
             end: "17:00",
           }}
@@ -165,8 +191,8 @@ const Page = () => {
           talk={{
             title: "Sprint Booths",
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            room: "Booths",
+              "Discussion some topics with project leader",
+            room: "R815",
             start: "16:00",
             end: "17:00",
           }}
