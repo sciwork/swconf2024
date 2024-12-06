bddfc542e52896f4a79e404b3e6ababbaa0386bb
rockleona <34214497+rockleona@users.noreply.github.com>
2024-12-06 11:58:20 +0800

    fix(menu): open a new tab when clicking previous conference

--- components/DrawerSecondaryLink.tsx
+++ components/DrawerSecondaryLink.tsx
@@ -8,7 +8,7 @@ const DrawerSecondaryLink = ({
   to: string;
 }) => {
   return (
-    <NextLink className="tw-block tw-w-full tw-font-yk" href={to}>
+    <NextLink className="tw-block tw-w-full tw-font-yk" href={to} target="_blank">
       <div className="tw-px-7 tw-py-2 tw-text-2xl tw-font-medium tw-leading-loose tw-text-gray-400 hover:tw-text-sky-400">
         {children}
       </div>
